df20411250953e044086712efe7fa2571c8eecd9
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-17 21:24:36 -0500

    get all

--- routes/items.js
+++ routes/items.js
@@ -3,5 +3,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('', (req, res, next) => {
+    try {
+        return res.json({ items: Item.findAll() });
+    } catch(err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
