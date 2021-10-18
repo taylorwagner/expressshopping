d44a95719dedfe4225ef9789017d8180fa1dbf94
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-10-17 21:12:37 -0500

    basic app set up

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
+const itemsRoutes = require('./routes/items');
 const ExpressError = ('./expressError');
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+app.use((req, res, next) => {
+    return new ExpressError("Not found", 404);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+
+    return res.json({
+        error: err.msg,
+    });
+});
+
+module.exports = app;
